d0d4f12775c15db01b168e7ab1e2b163efe2f255
robot-ie <mig.ruiz+1@gmail.com>
2024-11-24 13:54:03 +0000

    show xmas pattern

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+const showXmasPattern = () => {
+    isRed = false
+    for (let i = 0; i <= 51; i++) {
+        if (isRed) {
+            strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Red))
+        } else {
+            strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Green))
+        }
+        isRed = !(isRed)
+    }
+}
+let isRed = false
+let strip = neopixel.create(DigitalPin.P0, 51, NeoPixelMode.RGB)
+showXmasPattern();
+strip.show()
 basic.forever(function () {
-	
+    strip.rotate(1)
+    strip.show()
+    basic.pause(100)
 })
+
