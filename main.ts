607a66ff834db2d8143a0637b71c205f965f16e9
robot-ie <mig.ruiz+1@gmail.com>
2024-11-24 15:57:49 +0000

    modes

--- main.ts
+++ main.ts
@@ -9,17 +9,47 @@ const showXmasPattern = () => {
         isRed = !(isRed)
     }
 }
+const changeLedColors = () => {
+    const mode = modes[selectedModeIndex]
+    if (mode === 'blink') {
+        showXmasPattern();
+    }
+    else if (mode === 'full') {
+        strip.showColor(NeoPixelColors.Black)
+    }
+}
 let strip: neopixel.Strip
+const modes = ['blink', 'full']
+let selectedModeIndex = 0
 const init = () => {
     strip = neopixel.create(DigitalPin.P0, 51, NeoPixelMode.RGB)
-    showXmasPattern();
-    strip.show()
+    changeLedColors()
 }
 
 init()
-
 basic.forever(function () {
-    strip.rotate(1)
-    strip.show()
-    basic.pause(100)
+    const mode = modes[selectedModeIndex]
+    basic.showString(mode, 70)
 })
+basic.forever(function () {
+    const mode = modes[selectedModeIndex]
+    if (mode === 'blink') {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(200)
+    }
+    else if (mode === 'full') {
+        strip.showColor(NeoPixelColors.Red)
+        basic.pause(300)
+        strip.showColor(NeoPixelColors.Green)
+        basic.pause(300)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    selectedModeIndex = selectedModeIndex + 1
+    if (selectedModeIndex >= modes.length) {
+        selectedModeIndex = 0
+    }
+    changeLedColors()
+})
+
