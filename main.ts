01d67e1f9883d9a35e007cae445eb0ee501a0058
robot-ie <mig.ruiz+1@gmail.com>
2024-12-10 16:10:41 +0000

    Update pxt.json, main.blocks, main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,2 @@
-function init() {
-    
-    strip = neopixel.create(DigitalPin.P0, 51, NeoPixelMode.RGB)
-    shoXmasPattern()
-    strip.show()
-}
 
-function shoXmasPattern() {
-    let color: number;
-    
-    isRed = true
-    for (let i = 0; i < 51; i++) {
-        color = isRed ? NeoPixelColors.Red : NeoPixelColors.Green
-        strip.setPixelColor(i, neopixel.colors(color))
-        isRed = !isRed
-    }
-}
-
-let isRed = false
-let strip : neopixel.Strip = null
-init()
-basic.forever(function on_forever() {
-    strip.rotate(1)
-    strip.show()
-    basic.pause(100)
-})
+basic.showNumber(0)
