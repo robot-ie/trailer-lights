d409aa6044cd7f8fdb8aa8082ddc6b359f12a7c0
robot-ie <mig.ruiz+1@gmail.com>
2024-12-10 16:24:57 +0000

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,11 +1,10 @@
-
 namespace xmas {
-    export class OneLightPattern{
-        private strip:neopixel.Strip;
-        constructor(strip:neopixel.Strip){
+    export class OneLightPattern {
+        private strip: neopixel.Strip;
+        constructor(strip: neopixel.Strip) {
             this.strip = strip;
         }
-        init(){
+        init() {
             let isRed = true
             for (let i = 0; i < 51; i++) {
                 const color = isRed ? NeoPixelColors.Red : NeoPixelColors.Green
@@ -14,11 +13,11 @@ namespace xmas {
             }
         }
 
-        onForever(){
+        onForever() {
             strip.rotate(1)
             strip.show()
             basic.pause(1000)
         }
-        
+
     }
 }
