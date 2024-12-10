a1ecb6aeb88d3eb05758c35cc1227be135d72bb9
robot-ie <mig.ruiz+1@gmail.com>
2024-12-10 14:07:07 +0000

    python

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-basic.showNumber(0)
+function shoXmasPattern() {
+    let color: number;
+    let isRed = true
+    for (let i = 0; i < 51; i++) {
+        color = isRed ? NeoPixelColors.Red : NeoPixelColors.Green
+        strip.setPixelColor(i, neopixel.colors(color))
+        isRed = !isRed
+    }
+}
+
+function init() {
+    
+    strip = neopixel.create(DigitalPin.P0, 51, NeoPixelMode.RGB)
+    shoXmasPattern()
+    strip.show()
+}
+
+let strip : neopixel.Strip = null
+init()
