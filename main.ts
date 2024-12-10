5c6e7ea13b093f90084785b460c913895ac2add6
robot-ie <mig.ruiz+1@gmail.com>
2024-12-10 15:42:08 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-function shoXmasPattern() {
-    let color: number;
-    let isRed = true
-    for (let i = 0; i < 51; i++) {
-        color = isRed ? NeoPixelColors.Red : NeoPixelColors.Green
-        strip.setPixelColor(i, neopixel.colors(color))
-        isRed = !isRed
-    }
-}
-
 function init() {
     
     strip = neopixel.create(DigitalPin.P0, 51, NeoPixelMode.RGB)
@@ -15,9 +5,21 @@ function init() {
     strip.show()
 }
 
+function shoXmasPattern() {
+    let color: number;
+    
+    isRed = true
+    for (let i = 0; i < 51; i++) {
+        color = isRed ? NeoPixelColors.Red : NeoPixelColors.Green
+        strip.setPixelColor(i, neopixel.colors(color))
+        isRed = !isRed
+    }
+}
+
+let isRed = false
 let strip : neopixel.Strip = null
 init()
-basic.forever(function rotate() {
+basic.forever(function on_forever() {
     strip.rotate(1)
     strip.show()
     basic.pause(100)
